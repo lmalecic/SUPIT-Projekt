9f474c66fabc3cd586c90ca667040f95df5bcd53
lmalecic <luka.malecic@gmail.com>
2024-12-12 16:00:34 +0100

    v0.01

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-getElementById("nastavniPlan").classList.add("loggedIn");
+// Pričekaj da se učita prozor kako bi mogli dohvatiti elemente
+window.onload = function () {
+    // document.getElementById("nastavniPlan").classList.add("loggedIn");
+}
+
+function toggleNavBar() {
+    var element = document.getElementById("navbar");
+    if (element.style.display === "none") {
+        element.style.display = "flex";
+    } else {
+        element.style.display = "none";
+    }
+}
