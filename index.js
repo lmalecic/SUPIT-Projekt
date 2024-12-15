408e95800c085a4489c6eb139218d3ab3b43f809
lmalecic <162131791+lmalecic@users.noreply.github.com>
2024-12-15 16:52:37 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Pričekaj da se učita prozor kako bi mogli dohvatiti elemente
-window.onload = function () {
-    // document.getElementById("nastavniPlan").classList.add("loggedIn");
+window.onload = () => {
+    // document.getElementById("nastavniPlan").classList.add("isLoggedIn");
 }
 
 function toggleNavBar() {
     var element = document.getElementById("navbar");
-    if (element.style.display === "none") {
-        element.style.display = "flex";
+    if (element.classList.contains("expand")) {
+        element.classList.remove("expand");
     } else {
-        element.style.display = "none";
+        element.classList.add("expand");
     }
 }
