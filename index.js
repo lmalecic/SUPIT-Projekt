2f11b46e0bcb6f48c3bdb3890f7dd5604164a128
lmalecic <luka.malecic@gmail.com>
2024-12-18 08:54:49 +0100

    v0.01

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 // Pričekaj da se učita prozor kako bi mogli dohvatiti elemente
-window.onload = () => {
+window.onload = async () => {
     // document.getElementById("nastavniPlan").classList.add("isLoggedIn");
+    const elements = document.getElementsByClassName("typewriter")
+
+    // TypeWriter
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i]
+        element.setAttribute("step", "0")
+        await typeWriter(element)
+    }
+}
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms))
+}
+
+async function typeWriter(element) {
+    let txt = element.getAttribute("text")
+    let step = parseInt(element.getAttribute("step"))
+
+    if (step > txt.length) {
+        return true
+    }
+
+    if (step === 0) {
+        element.classList.add("text-appear")
+    }
+
+    element.innerHTML = txt.slice(0, step)
+    element.setAttribute("step", step + 1)
+
+    await timeout(40)
+    await typeWriter(element)
 }
 
 function toggleNavBar() {
-    var element = document.getElementById("navbar");
+    const element = document.getElementById("navbar")
     if (element.classList.contains("expand")) {
-        element.classList.remove("expand");
+        element.classList.remove("expand")
     } else {
-        element.classList.add("expand");
+        element.classList.add("expand")
     }
 }
