633bc9d91af281c779d3239ded1b3a0056b5cab3
lmalecic <162131791+lmalecic@users.noreply.github.com>
2025-01-19 21:40:06 +0100

    Added login dialog.

--- js/main.js
+++ js/main.js
@@ -7,13 +7,58 @@ function toggleNavBar() {
     }
 }
 
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve,ms))
+}
+
 function openLoginDialog() {
-    const element = document.createElement("div")
-    element.classList.add("login-dialog");
+    const overlay = document.createElement("div");
+    overlay.className = "dark-overlay"
 
+    const element = document.createElement("dialog");
+    element.className = "login-dialog";
 
+    const form = document.createElement("form");
 
-    document.body.appendChild(element);
+    const title = document.createElement("h2");
+    title.textContent = "Prijava";
+
+    const usernameLabel = document.createElement("label");
+    usernameLabel.textContent = "Korisničko ime";
+    usernameLabel.setAttribute("for", "username");
+    const usernameInput = document.createElement("input");
+    usernameInput.setAttribute("type", "text");
+    usernameInput.setAttribute("name", "username");
+
+    const passwordLabel = document.createElement("label");
+    passwordLabel.textContent = "Zaporka";
+    passwordLabel.setAttribute("for", "password");
+    const passwordInput = document.createElement("input");
+    passwordInput.setAttribute("type", "password");
+    passwordInput.setAttribute("name", "password");
+
+    const submitButton = document.createElement("input");
+    submitButton.setAttribute("type", "submit");
+    submitButton.setAttribute("value", "Prijavi se")
+
+    function closeDialog() {
+        overlay.remove();
+    }
+
+    overlay.addEventListener("click", closeDialog)
+
+    element.addEventListener("click", (event) => {
+        event.stopPropagation();
+    })
+
+    form.append(usernameLabel, usernameInput, passwordLabel, passwordInput, submitButton);
+    element.append(title, form);
+    overlay.appendChild(element);
+    document.body.appendChild(overlay);
+
+    timeout(10).then(() => {
+        overlay.classList.add("fade-in");
+    })
 }
 
 window.addEventListener("load", () => {
